2dc6aef65ba9b8270ac140fb29ebb29e70720483
gzhan0226 <gzhan@naver.com>
2025-05-04 02:50:31 +0900

    initial commit

---
+++ src/main/java/com/example/kwave/KwaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.kwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwaveApplication.class, args);
+	}
+
+}
