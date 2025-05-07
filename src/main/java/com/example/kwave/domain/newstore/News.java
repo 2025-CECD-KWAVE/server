ca3e74a11519d6e2c1f5daeec5f77408760050f0
taehwan <ltw0527@naver.com>
2025-05-07 17:20:17 +0900

    Add news entity

---
+++ src/main/java/com/example/kwave/domain/newstore/News.java
@@ -0,0 +1,53 @@
+package com.example.kwave.domain.newstore;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "news")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class News {
+
+    @Id
+    @Column(name = "news_id", nullable = false, unique = true)
+    private String newsId;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    @Column(name = "published_at")
+    private LocalDateTime publishedAt;
+
+    @Column(name = "enveloped_at")
+    private LocalDateTime envelopedAt;
+
+    @Column(name = "dateline")
+    private LocalDateTime dateline;
+
+    private String provider;
+
+    private String byline;
+
+    @Column(name = "provider_link_page")
+    private String providerLinkPage;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "news_category", joinColumns = @JoinColumn(name = "news_id"))
+    @Column(name = "category")
+    private List<String> category;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "news_incident_category", joinColumns = @JoinColumn(name = "news_id"))
+    @Column(name = "incident_category")
+    private List<String> categoryIncident;
+}
