761f2a21389c4d5d3b8caab9fb1305b98e78e706
tae0 <mickey470379@gmail.com>
2025-05-11 19:12:41 +0900

    refactor: 반환타입 및 변수명 수정

--- src/main/java/com/example/kwave/domain/user/dto/response/RecommendResponseDto.java
+++ src/main/java/com/example/kwave/domain/user/dto/response/RecommendResponseDto.java
@@ -2,8 +2,10 @@ package com.example.kwave.domain.user.dto.response;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class RecommendResponseDto {
     //추천 결과 뉴스 ID 반환
-    private String newsID;
+    private List<String> recommendedNewsIds;
 }
