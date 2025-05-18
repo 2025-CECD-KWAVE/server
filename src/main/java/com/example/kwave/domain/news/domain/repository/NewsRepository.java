43aa2a47e0764696c2e5265e62cd3f53a65ffe4d
tae0 <mickey470379@gmail.com>
2025-05-18 11:28:40 +0900

    refactor: 추천시 필요 함수 추가

--- src/main/java/com/example/kwave/domain/news/domain/repository/NewsRepository.java
+++ src/main/java/com/example/kwave/domain/news/domain/repository/NewsRepository.java
@@ -1,7 +1,14 @@
 package com.example.kwave.domain.news.domain.repository;
 
 import com.example.kwave.domain.news.domain.News;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface NewsRepository extends JpaRepository<News, String> {
+    @Query("select n from News n WHERE n.category = :category")
+    List<News> fetchLatestByCategory(String category, Pageable pageable);
+
 }
