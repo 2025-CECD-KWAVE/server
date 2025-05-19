d6c804c3f0ab6e7507cd0a20e3b6a061c2fbcc79
taehwan <ltw0527@naver.com>
2025-05-19 14:50:10 +0900

    refactor rest template to global config

--- src/main/java/com/example/kwave/global/config/RestTemplateConfig.java
+++ src/main/java/com/example/kwave/global/config/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.example.kwave.config;
+package com.example.kwave.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
