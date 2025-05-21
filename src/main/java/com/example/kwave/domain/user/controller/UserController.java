2d3cdaee3d9bd1e2af8c3605090db250e373ed5d
tae0 <mickey470379@gmail.com>
2025-05-21 13:37:03 +0900

    refactor: 시청 이력 업데이트 기능 추가

--- src/main/java/com/example/kwave/domain/user/controller/UserController.java
+++ src/main/java/com/example/kwave/domain/user/controller/UserController.java
@@ -1,10 +1,25 @@
 package com.example.kwave.domain.user.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.kwave.domain.user.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/{userId}/view")
+    public ResponseEntity<String> updateView(@PathVariable UUID userId,
+                                             @RequestBody List<String> categories) {
+        userService.updateViewedCategories(userId, categories);
+        return ResponseEntity.ok("시청 이력 업데이트");
+    }
 }
