b43e1829f00064096a6170b8503e53ebe7f10332
tae0 <mickey470379@gmail.com>
2025-05-30 15:55:43 +0900

    refactor: 사용자가 시청한 뉴스 카테고리 갱신 함수 추가

--- src/main/java/com/example/kwave/domain/news/service/NewsService.java
+++ src/main/java/com/example/kwave/domain/news/service/NewsService.java
@@ -4,10 +4,14 @@ import com.example.kwave.domain.news.domain.News;
 import com.example.kwave.domain.news.domain.repository.NewsRepository;
 import com.example.kwave.domain.news.dto.NewsDTO;
 import com.example.kwave.domain.news.external.NewsApiClient;
+import com.example.kwave.domain.user.domain.User;
+import com.example.kwave.domain.user.service.UserService;
+import com.example.kwave.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +19,8 @@ public class NewsService {
 
     private final NewsApiClient newsApiClient;
     private final NewsRepository newsRepository;
+    private final UserService userService;
+    private final UserRepository userRepository;
 
     public void saveNewsIfNotExists(News news) {
         if (!newsRepository.existsByNewsId(news.getNewsId())) {
@@ -32,5 +38,17 @@ public class NewsService {
                 System.out.println("이미 존재하는 뉴스: " + dto.getNewsId());
             }
         }
+    }
+
+    public void userWatched(UUID userId, String newsId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        News news = newsRepository.findById(newsId)
+                .orElseThrow(() -> new RuntimeException("News not found"));
+
+        List<String> categories = news.getCategory();
+
+        userService.updateViewedCategories(userId, categories);
     }
 }
