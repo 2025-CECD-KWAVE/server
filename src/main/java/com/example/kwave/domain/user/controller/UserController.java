8fb40366642f5ed560779905579f2a8993aa05f0
tae0 <mickey470379@gmail.com>
2025-06-01 02:00:07 +0900

    refactor: 사용되지 않는 코드 삭제

--- src/main/java/com/example/kwave/domain/user/controller/UserController.java
+++ src/main/java/com/example/kwave/domain/user/controller/UserController.java
@@ -8,12 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.kwave.domain.user.service.UserService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
