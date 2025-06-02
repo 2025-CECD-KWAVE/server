6c81f93ce7562244db608f30c3c454e289fa8f2e
tae0 <mickey470379@gmail.com>
2025-06-02 16:51:37 +0900

    refactor: 쿼리 수정

--- src/main/java/com/example/kwave/domain/news/domain/repository/NewsRepository.java
+++ src/main/java/com/example/kwave/domain/news/domain/repository/NewsRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,7 +21,8 @@ public interface NewsRepository extends JpaRepository<News, String> {
 
     Optional<News> findByNewsId(String newsId);
 
-    @Query("SELECT n FROM News n JOIN n.category c WHERE c = :category ORDER BY n.publishedAt DESC")
-    List<News> fetchLatestByCategory(String category, Pageable pageable);
+    @Query("SELECT DISTINCT n FROM News n JOIN n.category c WHERE c IN :categories ORDER BY n.publishedAt DESC")
+    List<News> fetchLatestByCategories(@Param("categories") List<String> categories, Pageable pageable);
+
 
 }
