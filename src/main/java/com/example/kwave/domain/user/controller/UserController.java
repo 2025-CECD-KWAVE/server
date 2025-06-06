da5dc5716338ac8fd35ded013fccc7861bb55ad8
tae0 <mickey470379@gmail.com>
2025-06-06 23:07:15 +0900

    refactor: 사용자가 추천한 뉴스 리스트 내에서 실제로 클릭했는지 확인하는 코드 추가

--- src/main/java/com/example/kwave/domain/user/controller/UserController.java
+++ src/main/java/com/example/kwave/domain/user/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.example.kwave.domain.user.controller;
 
+import com.example.kwave.domain.user.domain.CustomUserDetails;
+import com.example.kwave.domain.user.dto.request.ClickLogRequestDto;
 import com.example.kwave.domain.user.dto.request.SignupRequestDto;
 import com.example.kwave.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,4 +23,11 @@ public class UserController {
         return ResponseEntity.ok("회원가입 완료");
     }
 
+    //recommend 된 뉴스 열람했는지 확인하는 컨트롤러
+    @PostMapping("/click-log")
+    public ResponseEntity<String> clickLog (@AuthenticationPrincipal CustomUserDetails user, @RequestBody ClickLogRequestDto clickLogRequestDto) {
+        UUID userId = user.getId();
+        userService.updateClickLog(userId, clickLogRequestDto);
+        return ResponseEntity.ok("클릭 로그 저장 완료");
+    }
 }
