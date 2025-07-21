b14af42c2a2f1244c8f5855fdb1a4a1c064050e3
gzhan0226 <gzhan@naver.com>
2025-07-21 16:20:51 +0900

    docs: API 명세서 추가

--- src/main/java/com/example/kwave/domain/user/controller/RecommendController.java
+++ src/main/java/com/example/kwave/domain/user/controller/RecommendController.java
@@ -6,6 +6,8 @@ import com.example.kwave.domain.user.domain.CustomUserDetails;
 import com.example.kwave.domain.user.dto.request.RecommendRequestDto;
 import com.example.kwave.domain.user.dto.response.RecommendResponseDto;
 import com.example.kwave.domain.user.service.RecommendService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,11 +21,13 @@ import java.util.Locale;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/recommend")
+@Tag(name = "Recommend", description = "뉴스 추천 API")
 public class RecommendController {
     private final RecommendService recommendService;
     private final TranslatedNewsService translatedNewsService;
 
     @PostMapping
+    @Operation(summary = "추천된 뉴스 목록 조회", description = "내부 로직으로 뉴스 선별하여 뉴스 10개 반환")
     public ResponseEntity<RecommendResponseDto> recommend(@AuthenticationPrincipal CustomUserDetails user, Locale locale) {
         RecommendRequestDto requestDto = new RecommendRequestDto();
         requestDto.setUserId(user.getId());
