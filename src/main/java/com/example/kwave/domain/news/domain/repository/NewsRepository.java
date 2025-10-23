870cf2b4701e6a18c1b3fc60dee7c4df467bb30d
changtaeyoung <mickey470379@gmail.com>
2025-10-23 13:28:54 +0900

    refactor: 요약문이 null인 뉴스 찾는 함수 추가

--- src/main/java/com/example/kwave/domain/news/domain/repository/NewsRepository.java
+++ src/main/java/com/example/kwave/domain/news/domain/repository/NewsRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +25,5 @@ public interface NewsRepository extends JpaRepository<News, String> {
     @Query("SELECT DISTINCT n FROM News n JOIN n.category c WHERE c IN :categories")
     List<News> fetchLatestByCategories(@Param("categories") List<String> categories, Pageable pageable);
 
-
+    List<News> findBySummaryIsNullAndPublishedAtBetween(OffsetDateTime start, OffsetDateTime end);
 }
