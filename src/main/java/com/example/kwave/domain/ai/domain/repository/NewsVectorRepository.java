f568d7bfb7b6b0de1a3c4af55aab61c9eb881247
changtaeyoung <mickey470379@gmail.com>
2025-10-23 13:29:31 +0900

    refactor: opensearch에서 사용될 함수 추가

--- src/main/java/com/example/kwave/domain/ai/domain/repository/NewsVectorRepository.java
+++ src/main/java/com/example/kwave/domain/ai/domain/repository/NewsVectorRepository.java
@@ -4,8 +4,12 @@ import com.example.kwave.domain.ai.domain.NewsVectorDoc;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface NewsVectorRepository extends ElasticsearchRepository<NewsVectorDoc, String> {
+
+    Optional<NewsVectorDoc> findByNewsId(String newsId);
 
     boolean existsByNewsId(String newsId);
 
